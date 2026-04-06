c5a53575dcdbe0d49a12d76924d85768bf4fccea
Yasar Yigit Usta <yigitusta112@gmail.com>
2026-04-07 02:55:31 +0300

    factory

---
+++ Factory/YtkOdev/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+public interface IFutbolcuRapor
+{
+    void RaporuGoruntule();
+}
+public class FizikselRapor : IFutbolcuRapor
+{
+    public void RaporuGoruntule()
+    {
+        Console.WriteLine("Fiziksel Analiz: Hız: 92/100," +
+            " Dayanıklılık: Yüksek. Türkiye Süper Lig'e uygun.");
+    }
+}
+public class TeknikRapor : IFutbolcuRapor
+{
+    public void RaporuGoruntule()
+    {
+        Console.WriteLine("Teknik Analiz: Pas isabeti: 88%" +
+            ", Dribling: 90%. Türkiye Süper Lig'e uygun.");
+    }
+}
+public abstract class ScoutDepartmani
+{
+    public abstract IFutbolcuRapor RaporOlustur();
+
+    public void FutbolcuAnalizi()
+    {
+        var report= RaporOlustur();
+        Console.WriteLine("Futbolcu Analizi Raporu:");
+        report.RaporuGoruntule();
+    }
+}
+public class FizikselScout : ScoutDepartmani
+{
+    public override IFutbolcuRapor RaporOlustur()
+    {
+        return new FizikselRapor();
+    }
+}
+public class TeknikScout : ScoutDepartmani
+{
+    public override IFutbolcuRapor RaporOlustur()
+    {
+        return new TeknikRapor();
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        ScoutDepartmani departman;
+        departman = new FizikselScout();
+        departman.FutbolcuAnalizi();
+
+        departman = new TeknikScout();
+        departman.FutbolcuAnalizi();
+    }
+}
