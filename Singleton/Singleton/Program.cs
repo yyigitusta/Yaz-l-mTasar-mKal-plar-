3f94e45bceca39d703e258b4bc989ef4a7b6c0ef
Yasar Yigit Usta <yigitusta112@gmail.com>
2026-05-11 01:25:00 +0300

    Singleton

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,28 @@
+﻿public class VeritabanıBaglantisi
+{
+    private static VeritabanıBaglantisi _ornegi;
+    private static readonly object _kilit = new object();
+
+    private VeritabanıBaglantisi() { }
+
+    public static VeritabanıBaglantisi OrnekAl()
+    {
+        if (_ornegi == null)
+        {
+            lock (_kilit) 
+            {
+                if (_ornegi == null)
+                {
+                    _ornegi = new VeritabanıBaglantisi();
+                    Console.WriteLine("Veritabanı bağlantısı ilk kez oluşturuldu.");
+                }
+            }
+        }
+        return _ornegi;
+    }
+
+    public void SorguCalistir(string sorgu)
+    {
+        Console.WriteLine($"Sorgu çalıştırılıyor: {sorgu}");
+    }
+}
